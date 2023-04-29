1e44251996f3b3c84d361dbd56f6e0181cfd2981
orenlitmanovich <120041629+orenlitmanovich@users.noreply.github.com>
2023-04-29 12:42:37 +0300

    changed header

--- frontend/src/Components/LayoutArea/Header/Header.tsx
+++ frontend/src/Components/LayoutArea/Header/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header(): JSX.Element {
     return (
         <div className="Header">
-            <h1>Header...</h1>
+            <h1>Gift Shop🎁</h1>
         </div>
     );
 }
