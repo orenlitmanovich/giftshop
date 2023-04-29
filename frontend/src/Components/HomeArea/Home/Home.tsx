3d402229f30cdb18a64a6122c15ac040db5f0d16
orenlitmanovich <120041629+orenlitmanovich@users.noreply.github.com>
2023-04-29 13:05:52 +0300

    changed home page and css added image

--- frontend/src/Components/HomeArea/Home/Home.tsx
+++ frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,9 +1,13 @@
 import "./Home.css";
-
+import gift from "../../../Assets/Images/gifts-isolated.jpg"
 function Home(): JSX.Element {
     return (
         <div className="Home">
-            <h2>Home...</h2>
+            <h2>Gift Shop Home Page</h2>
+            <p>Best Gifts For Any Age</p>
+            <hr/>
+            <img src={gift}/>
+            
 
         </div>
     );
