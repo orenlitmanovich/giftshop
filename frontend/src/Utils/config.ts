c52d3f19aaf306cdf32be93e75dce4a48ebbbc1e
orenlitmanovich <120041629+orenlitmanovich@users.noreply.github.com>
2023-04-29 12:38:15 +0300

    config front end

--- frontend/src/Utils/config.ts
+++ frontend/src/Utils/config.ts
@@ -1,5 +1,7 @@
 class Config {
-    public serverUrl = "http://localhost:3001/api/____";
+    public targetAudienceUrl = "http://localhost:3001/api/target-audience";
+    public giftsPerTargetAudienceUrl="http://localhost:3001/api/gift-per-target-audience/";
+    public giftsUrl="http://localhost:3001/api/gifts";
 }
 
 const appConfig = new Config(); // Singleton
